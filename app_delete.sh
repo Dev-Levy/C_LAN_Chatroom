f2195574717e01a5ee80b37c4ed8a844f232a426
shallowboii <143343678+shallowboii@users.noreply.github.com>
2025-04-30 16:42:21 +0200

    Update app_delete.sh

--- app_delete.sh
+++ app_delete.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Change directory into build/char_device
 cd "$SCRIPT_DIR/build/char_device/" || exit 1
 
 sudo rmmod char_dev.ko
-
-
+# out one folder is char_device folder one 
+cd ../..
 # Change directory into the Makefiles folder
 cd "$SCRIPT_DIR/Makefiles" || exit 1
 
